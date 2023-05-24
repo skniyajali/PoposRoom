1a58ccdced02be512f84b7cdf38ff626801aa20c
Sk Niyaj Ali <niyaj639@gmail.com>
2023-05-24 13:50:58 +0530

    - Added All Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,26 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.appsweep)
+    alias(libs.plugins.ksp)
+    id(libs.plugins.kotlin.kapt.get().pluginId)
+    alias(libs.plugins.androidx.baselineprofile)
 }
 
 android {
-    namespace = "com.niyaj.poposroom"
-    compileSdk = 33
+    namespace = libs.versions.namespace.get()
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.niyaj.poposroom"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = libs.versions.namespace.get()
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+        testInstrumentationRunner = "com.niyaj.popos.HiltTestRunner"
+        manifestPlaceholders.putAll(mapOf("sentryEnvironment" to "production"))
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -31,43 +36,187 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.4"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/LICENSE.md"
+            excludes += "META-INF/LICENSE-notice.md"
+            excludes += "DebugProbesKt.bin"
         }
     }
 }
 
 dependencies {
-
     implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
+
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
     implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.material.icons)
+    implementation(libs.ui.tooling.preview)
 
+    implementation(libs.activity.compose)
+
+    //Startup & Splash screen
+    implementation(libs.startup)
+    implementation(libs.splashscreen)
+
+    implementation(libs.runtime.livedata)
+
+    // ViewModel
+    implementation(libs.viewmodel.ktx)
+    implementation(libs.viewmodel.compose)
+    implementation(libs.runtime.compose)
+    implementation(libs.runtime.ktx)
+    // Saved state module for ViewModel
+    implementation(libs.viewmodel.savedstate)
+    // Annotation processor
+    implementation(libs.common.java8)
+
+    // Compose dependencies
+    implementation(libs.navigation.compose)
+
+    // Kotlin + coroutines
+    implementation(libs.work.runtime.ktx)
+    androidTestImplementation(libs.work.testing)
+
+
+    //Accompanist
+    implementation(libs.flowlayout)
+    implementation(libs.systemuicontroller)
+    implementation(libs.permissions)
+    implementation(libs.swiperefresh)
+    implementation(libs.placeholder.material)
+    implementation(libs.pager)
+    implementation(libs.pager.indicators)
+
+    // Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // For testing
+    testImplementation(libs.coroutines.test)
+    androidTestImplementation(libs.coroutines.test)
+
+    //Hilt Work
+    implementation(libs.hilt.work)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.android)
+
+
+    // Dagger & Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.dagger.compiler)
+
+    // For testing
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.dagger.compiler)
+
+    testImplementation(libs.hilt.android.testing)
+    kaptTest(libs.hilt.dagger.compiler)
+
+    // Timber
+    implementation(libs.timber)
+
+    //RevealSwipe
+    implementation(libs.revealswipe)
+
+    //Pos.printer
+    implementation(libs.pos.printer)
+
+    //Realm
+    // implementation(libs.realm.library.base)
+
+    // Room
+    implementation(libs.room.runtime)
+    ksp(libs.room.complier)
+
+    // To use Kotlin annotation processing tool (kapt)
+    ksp(libs.room.complier)
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.room.ktx)
+
+    // optional - Test helpers
+    testImplementation(libs.room.testing)
+
+    // optional - Paging 3 Integration
+    implementation(libs.room.paging)
+
+    // Paging
+    implementation(libs.paging.runtime)
+    // optional - Jetpack Compose integration
+    implementation(libs.paging.compose)
+
+    // debugImplementation because LeakCanary should only run in debug builds.
+    debugImplementation(libs.leakcanary)
+
+    //Compose Material Dialogs
+    implementation(libs.dialog.core)
+    implementation(libs.dialog.datetime)
+
+    //RaamCosta Library
+    implementation(libs.raamcosta.animation.core)
+    ksp(libs.raamcosta.ksp)
+
+    //Moshi
+    ksp(libs.moshi.kotlin.codegen)
+    implementation(libs.moshi)
+
+    //ProfileInstaller
+    implementation(libs.profileinstaller)
+
+    // Local unit tests
     testImplementation(libs.junit)
+    androidTestImplementation(libs.junit)
 
-    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
+    testImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.core.ktx)
+    androidTestImplementation(libs.androidx.test.runner)
 
+    testImplementation(libs.androidx.arch.core.testing)
+    androidTestImplementation(libs.androidx.arch.core.testing)
+
+    androidTestImplementation(libs.ui.test.junit)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    // Truth
+    testImplementation(libs.truth)
+    androidTestImplementation(libs.truth)
+
+    //Mockk
+    androidTestImplementation(libs.mockk.android)
+    testImplementation(libs.mockk)
+
+    //ACRA Logger
+    implementation(libs.acra.mail)
+    implementation(libs.acra.toast)
+    implementation(libs.acra.notification)
+    implementation(libs.acra.limiter)
+    implementation(libs.acra.advanced.scheduler)
+
+    //Sentry
+    implementation(libs.sentry.android)
+    implementation(libs.sentry.compose.android)
+
+    //Baseline Profile
+//    "baselineProfile"(project(mapOf("path" to ":benchmark")))
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.sentry)
+    alias(libs.plugins.androidx.baselineprofile) apply false
+    alias(libs.plugins.androidx.benchmark) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
