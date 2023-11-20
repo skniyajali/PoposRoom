0890b931423a5f31875d20134d00f24c8141b2bd
Sk Niyaj Ali <niyaj639@gmail.com>
2023-11-21 05:04:35 +0530

    Changed JavaVersion to Match Baseline Profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,11 @@ android {
         buildConfig = true
     }
 
+    compileOptions {
+        sourceCompatibility  = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
     packaging {
         resources {
             excludes.add("/META-INF/{AL2.0,LGPL2.1}")
