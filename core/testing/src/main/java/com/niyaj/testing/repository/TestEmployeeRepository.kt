a5cb5ee320414f1129ecc491dd0e57e5ae6ffa2a
Sk Niyaj Ali <niyaj639@gmail.com>
2024-08-08 10:09:13 +0530

    - Fixed Formatting

--- core/testing/src/main/java/com/niyaj/testing/repository/TestEmployeeRepository.kt
+++ core/testing/src/main/java/com/niyaj/testing/repository/TestEmployeeRepository.kt
@@ -1,17 +1,18 @@
 /*
- *      Copyright 2024 Sk Niyaj Ali
+ * Copyright 2024 Sk Niyaj Ali
  *
- *      Licensed under the Apache License, Version 2.0 (the "License");
- *      you may not use this file except in compliance with the License.
- *      You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *              http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the License is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the License for the specific language governing permissions and
- *      limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package com.niyaj.testing.repository
@@ -33,7 +34,7 @@ import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.update
 import org.jetbrains.annotations.TestOnly
 
-class TestEmployeeRepository: EmployeeRepository {
+class TestEmployeeRepository : EmployeeRepository {
 
     /**
      * The backing employee list for testing
@@ -140,7 +141,7 @@ class TestEmployeeRepository: EmployeeRepository {
     }
 
     fun createTestItem(): Employee {
-        val newEmployee =  Employee(
+        val newEmployee = Employee(
             employeeId = 1,
             employeeName = "Test Employee",
             employeePhone = "1234567890",
